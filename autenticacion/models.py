9667ddae58e9f5c115beb792f34e4a7baa291111
Alexander ▲▼ <hs19011@ues.edu.sv>
2022-10-27 01:41:44 -0600

    usuarios by alexander

--- autenticacion/models.py
+++ autenticacion/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Usuario(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    es_secretaria = models.BooleanField(default=False, help_text='Indica si es secretaria')
+    es_jefatura = models.BooleanField(default=False, help_text='Indica si es jefatura')
+    es_cajera = models.BooleanField(default=False, help_text='Indica si es cajera')
+    es_agente = models.BooleanField(default=False, help_text='Indica si es agente')
+    es_asociado = models.BooleanField(default=False, help_text='Indica si es asociado')
+
+    def __str__(self):
+        return "El Usuario extendido depende del User: " + self.user.pk.__str__() + " - " + self.user.username
+
--- webApp/views.py
+++ webApp/views.py
@@ -1,9 +1,80 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from cliente.models import *
+from autenticacion.models import *
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 # Create your views here.
 
 
+def obtnerUsuario(request):
+    usuario = Usuario.objects.get(pk=request.user.pk)
+
+    return usuario
+
+
 def home(request):
+    if obtnerUsuario(request).es_secretaria:
+        return redirect('vista_secretaria')
+
+    if obtnerUsuario(request).es_jefatura:
+        return redirect('vista_jefatura')
+
+    if obtnerUsuario(request).es_cajera:
+        return redirect('vista_cajera')
+
+    if obtnerUsuario(request).es_agente:
+        return redirect('vista_agente')
+
+    if obtnerUsuario(request).es_asociado:
+        return redirect('vista_asociado')
+
     return render(request, 'paginas/home.html')
+
+
+@login_required
+def vista_secretaria(request):
+    if not obtnerUsuario(request).es_secretaria:
+        messages.warning(request, "Este apartado es solo para secretario/a")
+        return redirect('home')
+
+    return render(request, 'paginas/secretaria.html')
+
+
+@login_required
+def vista_jefatura(request):
+    if not obtnerUsuario(request).es_jefatura:
+        messages.warning(request, "Este apartado es solo para jefatura")
+        return redirect('home')
+
+    return render(request, 'paginas/jefatura.html')
+
+
+@login_required
+def vista_cajera(request):
+    if not obtnerUsuario(request).es_cajera:
+        messages.warning(request, "Este apartado es solo para cajero/a")
+        return redirect('home')
+
+    return render(request, 'paginas/cajera.html')
+
+
+@login_required
+def vista_agente(request):
+    if not obtnerUsuario(request).es_agente:
+        messages.warning(request, "Este apartado es solo para un agente")
+        return redirect('home')
+
+    return render(request, 'paginas/agente.html')
+
+
+@login_required
+def vista_asociado(request):
+    if not obtnerUsuario(request).es_asociado:
+        messages.warning(request, "Este apartado es solo para un asociado")
+        return redirect('home')
+
+    return render(request, 'paginas/asociado.html')
+
+
 def documentos_anexos(request):
     return render(request, 'consultar_documentos_anexos/consultar.html')
