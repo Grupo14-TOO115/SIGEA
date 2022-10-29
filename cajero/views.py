81298ad44930fcea6b87a0e0ed494dae8b8fd526
Alexander ▲▼ <hs19011@ues.edu.sv>
2022-10-29 13:42:24 -0600

    bug de toruño arreglao

--- cajero/views.py
+++ cajero/views.py
@@ -7,25 +7,26 @@ from django.contrib.auth.decorators import login_required
 from cliente.models import *
 from .models import *
 
+
 def lista_solicitudes_pago(request):
     busqueda = request.POST.get("buscar")
-    solicitudes = SolicitudPago.objects.filter(pagado =  False)
+    solicitudes = SolicitudPago.objects.filter(pagado=False)
 
     if busqueda:
         solicitudes = SolicitudPago.objects.filter(
-            Q(id_cliente__identificacion__icontains = busqueda) |
-            Q(id_cliente__nombres__icontains = busqueda) |
-            Q(id_cliente__apellidos__icontains = busqueda)
+            Q(pagado=False) & (
+            Q(id_cliente__identificacion__icontains=busqueda) |
+            Q(id_cliente__nombres__icontains=busqueda) |
+            Q(id_cliente__apellidos__icontains=busqueda))
         ).distinct()
 
     return render(request, 'lista_solicitudes_pago.html', {'solicitudes': solicitudes})
+
 
 def confirmar_pago(request, id_cliente):
     cliente = Cliente.objects.get(id_cliente = id_cliente)
     cliente.es_asociado = True
     cliente.save()
-
-
 
     solicitudPagada = SolicitudPago.objects.get(id_cliente = cliente)
     solicitudPagada.pagado = True
